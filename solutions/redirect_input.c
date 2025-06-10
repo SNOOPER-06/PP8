a08813a84d12217be8c4ebaaf3dbda6ef981d0eb
SNOOPER-06 <marc.faton@stud.thga.de>
2025-06-11 01:00:00 +0200

    Add redirect_input

---
+++ solutions/redirect_input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define BUF_SIZE 256
+
+int main(void) {
+    char buf[BUF_SIZE];
+    if (fgets(buf, BUF_SIZE, stdin))
+        printf("You entered: %s", buf);
+    return 0;
+}
