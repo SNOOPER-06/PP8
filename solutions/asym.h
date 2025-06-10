ad2ebe95e3b00cf756d4dfff45afdbb401dbfc11
SNOOPER-06 <marc.faton@stud.thga.de>
2025-06-11 00:52:50 +0200

    Remove all files from Git index

--- solutions/asym.h
+++
@@ -1,5 +0,0 @@
-#ifndef ASYM_H
-#define ASYM_H
-char encrypt_xor(char c, char key);
-char decrypt_xor(char c, char key);
-#endif
--- solutions/cipher.h
+++
@@ -1,5 +0,0 @@
-#ifndef CIPHER_H
-#define CIPHER_H
-char encrypt_char(char c, int shift);
-char decrypt_char(char c, int shift);
-#endif
